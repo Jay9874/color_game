8fbca252822eaf8f4d72d7ca5fdb6b785dd940f1
Jay9874 <jayprakashsharma225@gmail.com>
2022-11-20 02:33:56 +0530

    added a start button

--- script.js
+++ script.js
@@ -1,86 +1,83 @@
-var currentLevel = 0;
-var levelText = "Level ";
-var buttonsPressed = [];
-var buttonColors = ["red", "green", "yellow", "blue"];
-var hasGameStarted = true;
-var gameOver = false;
-var userClickCounter = 0;
+var currentLevel = 0
+var levelText = 'Level '
+var buttonsPressed = []
+var buttonColors = ['red', 'green', 'yellow', 'blue']
+var hasGameStarted = true
+var gameOver = false
+var userClickCounter = 0
+var initialTitle = $('#level-title').text()
+var wrongInputAlarm = new Audio('sounds/alarm.mp3')
 
-$(document).on('keydown', function(event){
-if(!gameOver){
-    $('#level-title').text(levelText+currentLevel);
-    if(hasGameStarted){
-    pressButton();
+$('.action-btn').on('click', function (event) {
+  if (!gameOver) {
+    console.log("start codes are executed.");
+    $('#level-title').text(levelText + currentLevel)
+    if (hasGameStarted) {
+      $('.action-btn').hide()
+      pressButton()
     }
-}
-else {
+  } else {
     console.log('Game over.')
+  }
+})
+
+function pressButton () {
+  var randomNumber = Math.floor(Math.random() * 4)
+  var prsdBtnClr = buttonColors[randomNumber]
+  buttonsPressed.push(prsdBtnClr)
+  makeSound(prsdBtnClr)
+  animatePress(prsdBtnClr)
+  hasGameStarted = false
 }
-});
-
-
-function pressButton(){
-var randomNumber = Math.floor(Math.random()*4);
-var prsdBtnClr = buttonColors[randomNumber];
-buttonsPressed.push(prsdBtnClr);
-console.log("buttons pressed are: "+buttonsPressed);
-makeSound(prsdBtnClr);
-animatePress(prsdBtnClr);
-hasGameStarted = false;
-}
-
-$('.btn').on('click', function(event){
-if(!gameOver){
-    var userChosedColor = this.id;
-    makeSound(userChosedColor);
+$('.btn').on('click', function (event) {
+  if (!gameOver) {
+    console.log('click button got executed.');
+    var userChosedColor = this.id
+    makeSound(userChosedColor)
     animatePress(userChosedColor)
-    compareSequence(userChosedColor);
-}
-else {
+    compareSequence(userChosedColor)
+  } else {
     console.log('Game over.')
-}
-});
+  }
+})
 
-
-function makeSound(btnClr){
-var audioPath = 'sounds/'+btnClr+'.mp3';
-var audio = new Audio(audioPath);
-audio.play();
-}
-
-function animatePress(btnClr){
-$("."+btnClr).addClass('pressed');
-setTimeout(function() {
-    $('.'+btnClr).removeClass('pressed');
-}, 100);
+function makeSound (btnClr) {
+  var audioPath = 'sounds/' + btnClr + '.mp3'
+  var audio = new Audio(audioPath)
+  audio.play()
 }
 
-function compareSequence(clickedColor){
-console.log("clicks: "+userClickCounter);
-if(clickedColor != buttonsPressed[userClickCounter]){
-    wrongSequence();
-}
-else if(userClickCounter == buttonsPressed.length-1){
-    nextLevel();
-}
-else{
-    userClickCounter++;
-}
+function animatePress (btnClr) {
+  $('.' + btnClr).addClass('pressed')
+  setTimeout(function () {
+    $('.' + btnClr).removeClass('pressed')
+  }, 100)
 }
 
-function nextLevel(){
-userClickCounter = 0;
-currentLevel++;
-hasGameStarted = true;
-setTimeout(function(){
-    pressButton();
-    $('#level-title').text(levelText+currentLevel);
-}, 500)
+function compareSequence (clickedColor) {
+  if (clickedColor != buttonsPressed[userClickCounter]) {
+    wrongSequence()
+  } else if (userClickCounter == buttonsPressed.length - 1) {
+    nextLevel()
+  } else {
+    userClickCounter++
+  }
 }
 
-function wrongSequence(){
-    makeSound('alarm');
-    $('body').addClass('game-over');
-    gameOver = true;
-    $('#level-title').text('Wrong Color, GAME OVER, refresh page to restart.');
+function nextLevel () {
+  userClickCounter = 0
+  currentLevel++
+  hasGameStarted = true
+  setTimeout(function () {
+    pressButton()
+    $('#level-title').text(levelText + currentLevel)
+  }, 500)
 }
+
+function wrongSequence () {
+  wrongInputAlarm.play()
+  $('body').addClass('game-over')
+  gameOver = true
+  $('#level-title').html('Wrong Color <br><br>GAME OVER <br><br>refresh to play again.');
+}
+
